082bd81385c0a4163a8ad7ea536081be6eec8421
nafissmf <100180243+nafissmf@users.noreply.github.com>
2023-03-14 23:59:48 +0700

    Update MPoligon.java

--- Praktikum 3/org/main/MPoligon.java
+++ Praktikum 3/org/main/MPoligon.java
@@ -10,12 +10,12 @@ package org.main;
 import org.bangundatar.*;
 
 public class MPoligon{
-	     public static void main(String[] args){
-		          PersegiPanjang persegi = new PersegiPanjang(10,10,4);
-	          	Segitiga segitiga = new Segitiga(8,6,10,12);
-	          	persegi.printInfo();
-		          segitiga.printInfo();
-	          	System.out.println("Luas Persegi Panjang : "+persegi.hitungLuas());
-		          System.out.println("Luas Segitiga : "+segitiga.hitungLuas());
-	     }
+	public static void main(String[] args){
+		PersegiPanjang persegi = new PersegiPanjang(10,10,4);
+		Segitiga segitiga = new Segitiga(8,6,10,12);
+		persegi.printInfo();
+		segitiga.printInfo();
+		System.out.println("Luas Persegi Panjang : "+persegi.hitungLuas());
+		System.out.println("Luas Segitiga : "+segitiga.hitungLuas());
+	}
 }
