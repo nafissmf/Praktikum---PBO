11557e75fe1df7a76848c2e711e369d3151a62f3
nafissmf <100180243+nafissmf@users.noreply.github.com>
2023-03-14 23:59:28 +0700

    Update MPoligon.java

--- Praktikum 3/org/main/MPoligon.java
+++ Praktikum 3/org/main/MPoligon.java
@@ -5,17 +5,17 @@
 * Deskripsi : Driver class untuk poligon, persegi panjang, dan segitiga
 */
 
-package org.main; 
- 
-import org.bangundatar.*; 
- 
-public class MPoligon{ 
-    public static void main(String[] args){ 
-        PersegiPanjang persegi = new PersegiPanjang(10,10,4);
-        Segitiga segitiga = new Segitiga(7,24,25,3);
-        persegi.printInfo();
-        System.out.println("Luas Persegi Panjang : "+persegi.hitungLuas());
-        segitiga.printInfo();
-        System.out.println("Panjang Sisi Miring adalah : "+segitiga.Pythagoras()); 
-    } 
+package org.main;
+
+import org.bangundatar.*;
+
+public class MPoligon{
+	     public static void main(String[] args){
+		          PersegiPanjang persegi = new PersegiPanjang(10,10,4);
+	          	Segitiga segitiga = new Segitiga(8,6,10,12);
+	          	persegi.printInfo();
+		          segitiga.printInfo();
+	          	System.out.println("Luas Persegi Panjang : "+persegi.hitungLuas());
+		          System.out.println("Luas Segitiga : "+segitiga.hitungLuas());
+	     }
 }
