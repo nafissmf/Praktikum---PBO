bffd125a39a06056a16db8cbfcc94fa23260bf50
nafissmf <100180243+nafissmf@users.noreply.github.com>
2023-03-15 00:00:25 +0700

    Update segitiga.java

--- Praktikum 3/org/bangundatar/segitiga.java
+++ Praktikum 3/org/bangundatar/segitiga.java
@@ -5,26 +5,24 @@
 * Deskripsi : Representasi dari objek segitiga, turunan kelas poligon
 */
 
-package org.bangundatar; 
- 
-import org.poligon.Poligon; 
- 
-public class Segitiga extends Poligon{ 
-    private double alas, tinggi, sisiMiring; 
- 
-    public Segitiga(double alas, double tinggi, double sisiMiring, int jumlahSisi){ 
-        this.alas = alas; 
-        this.tinggi = tinggi; 
-        this.sisiMiring = sisiMiring; 
-        this.jumlahSisi = jumlahSisi; 
-    } 
- 
-    public double Pythagoras(){ 
-        sisiMiring = Math.sqrt(alas * alas + tinggi * tinggi); 
-        return sisiMiring; 
-    } 
- 
-    public void printInfo(){ 
-        System.out.println("Bangun Segitiga bersisi "+this.getJumlahSisi()); 
-    } 
+package org.bangundatar;
+
+import org.poligon.Poligon;
+
+public class Segitiga extends Poligon{
+	private double alas, tinggi;
+	
+	public Segitiga(double alas,double tinggi, int jumlahSisi){
+		this.alas = alas;
+		this.tinggi = tinggi;
+		this.jumlahSisi = jumlahSisi;
+	}
+	
+	public double hitungLuas(){
+		return 0.5 * alas * tinggi;
+	}
+	
+	public void printInfo(){
+		System.out.println("Bangun Segitiga bersisi "+this.getJumlahSisi());
+	}
 }
